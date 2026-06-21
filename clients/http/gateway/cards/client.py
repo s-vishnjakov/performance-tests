5410cbb50edff89673a94ec9b17d449a4b894e54
s-vishnjakov <su32.ch@gmail.com>
2026-06-21 18:10:27 +0300

    fix descriptions in cards client

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -15,8 +15,18 @@ class CardsGatewayHTTPClient(HTTPClient):
     """
     Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
     """
-    def issue_virtual_card_api(self, request):
+    def issue_virtual_card_api(self, request: IssueCardDict) -> Response:
+        """
+        Выпуск новой виртуальной карты.
+        :param request: Словарь с данными ID пользователя и ID счета.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
         return self.post("/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request):
-        return self.post("/api/v1/cards/issue-physical-card ", json=request)
+    def issue_physical_card_api(self, request: IssueCardDict) -> Response:
+        """
+        Выпуск новой физической карты.
+        :param request: Словарь с данными ID пользователя и ID счета.
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post("/api/v1/cards/issue-physical-card", json=request)
