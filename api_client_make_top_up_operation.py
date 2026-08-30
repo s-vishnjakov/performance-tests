decb19f6fd91ae3e8820642a8e71516ffb7252f9
s-vishnjakov <su32.ch@gmail.com>
2026-08-30 18:19:58 +0300

    Pydantic in HTTP API clients: add ./users/schema and modify UsersGatewayHTTPClient + scripts in accordance

--- api_client_make_top_up_operation.py
+++ api_client_make_top_up_operation.py
@@ -11,7 +11,7 @@ create_user_response = users_gateway_client.create_user()
 print('Create user response:', create_user_response)
 
 open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(
-    user_id=create_user_response["user"]["id"]
+    user_id=create_user_response.user.id
 )
 print('Open debit card account response:', open_debit_card_account_response)
 
---
+++ clients/http/gateway/users/schema.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+from pydantic.alias_generators import to_camel
+
+
+class UserSchema(BaseModel):
+    """Schema for user"""
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias='lastName')
+    first_name: str = Field(alias='firstName')
+    middle_name: str = Field(alias='middleName')
+    phone_number: str = Field(alias='phoneNumber')
+
+
+class GetUserResponseSchema(BaseModel):
+    """Response schema for getting the user"""
+    user: UserSchema
+
+
+class CreateUserRequestSchema(BaseModel):
+    """Request schema for creating a new user"""
+    model_config = ConfigDict(validate_by_name=True)
+
+    email: EmailStr
+    last_name: str = Field(alias='lastName')
+    first_name: str = Field(alias='firstName')
+    middle_name: str = Field(alias='middleName')
+    phone_number: str = Field(alias='phoneNumber')
+
+
+class CreateUserResponseSchema(BaseModel):
+    """Response schema for creating a new user"""
+    user: UserSchema
