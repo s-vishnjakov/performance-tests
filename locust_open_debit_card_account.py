f1e3bea47d3b03bec9e332cc5a865fb63012937a
s-vishnjakov <su32.ch@gmail.com>
2026-09-18 11:31:37 +0300

    Locust intro: performance scenario - open debit card account

---
+++ locust_open_debit_card_account.py
@@ -0,0 +1,21 @@
+from locust import HttpUser, between, task
+from tools.fakers import fake
+
+
+class OpenDebitCardAccountScenarioUser(HttpUser):
+    wait_time = between(1, 3)
+
+    def on_start(self) -> None:
+        request = {
+            "email": fake.email(),
+            "lastName": fake.last_name(),
+            "firstName": fake.first_name(),
+            "middleName": fake.middle_name(),
+            "phoneNumber": fake.phone_number()
+        }
+        response = self.client.post("/api/v1/users", json=request)
+        self.user_id = {"userId": response.json()['user']['id']}
+
+    @task
+    def open_debit_card_account(self):
+        self.client.post("/api/v1/accounts/open-debit-card-account", json=self.user_id)
